2070b5faff2a6fc27aa879ea60265e6b102d4358
Jos Klever <josklever@gmail.com>
2022-07-24 14:05:52 +0200

    Update convert-myisam-to-innodb.sql

--- convert-myisam-to-innodb.sql
+++ convert-myisam-to-innodb.sql
@@ -12,10 +12,11 @@ ORDER BY table_schema, table_name;
 
 -- OPTION 2
 -- Set your current database in the variable and select only the tables of this database to convert to INNODB.
+
 SET @DATABASE_NAME = 'name_of_your_db';
 SELECT  CONCAT('ALTER TABLE ', table_name, ' ENGINE=InnoDB;') AS sql_statements
 FROM    information_schema.tables AS tb
 WHERE   table_schema = @DATABASE_NAME
 AND     `ENGINE` = 'MyISAM'
 AND     `TABLE_TYPE` = 'BASE TABLE'
-ORDER BY table_name DESC;
+ORDER BY table_name;
