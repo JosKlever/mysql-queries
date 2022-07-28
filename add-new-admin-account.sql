86a5464d9d48a6cd2754d8dc37d3262ca3a151dd
Jos Klever <josklever@gmail.com>
2022-07-28 14:00:38 +0200

    Update add-new-admin-account.sql
    Small fix for user_nicename, which should be the username in lowercase and with spaces replaced by dashes.

--- add-new-admin-account.sql
+++ add-new-admin-account.sql
@@ -6,7 +6,7 @@ SET @USER_ID = (SELECT AUTO_INCREMENT FROM information_schema.tables WHERE TABLE
 SET @PASSWORD = 'XXXXXXXXXXXXXX';
 
 INSERT INTO wp_users (ID, user_login, user_pass, user_nicename, user_email, user_url, user_registered, user_activation_key, user_status, display_name)
-VALUES (@USER_ID, 'UserName', MD5(@PASSWORD), 'User Nice Name', 'info@userdomainname.com', 'https://userdomainname.com/', NOW(), '', '0', 'User Display Name');
+VALUES (@USER_ID, 'UserName', MD5(@PASSWORD), 'username', 'info@userdomainname.com', 'https://userdomainname.com/', NOW(), '', '0', 'User Display Name');
 
 INSERT INTO wp_usermeta (umeta_id, user_id, meta_key, meta_value) VALUES (NULL, @USER_ID, 'wp_capabilities', 'a:1:{s:13:"administrator";b:1;}');
 
